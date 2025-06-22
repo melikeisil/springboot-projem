2a3cced886ab2eddc5a31265027941309e64d09d
melikeisil <melikeisilutal@gmail.com>
2025-06-22 18:33:53 +0300

    Update Wishlist.java

--- ownify/src/main/java/com/ownify/Entity/Wishlist.java
+++ ownify/src/main/java/com/ownify/Entity/Wishlist.java
@@ -32,7 +32,7 @@ public class Wishlist {
     @Column(name = "created_at")
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    // ✅ Constructors eklendi
+  
     public Wishlist() {}
 
     public Wishlist(User user, Product product) {
@@ -40,7 +40,7 @@ public class Wishlist {
         this.product = product;
     }
 
-    // Getters and Setters
+  
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
